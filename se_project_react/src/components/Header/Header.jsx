7545aa47a83274e3dda6ce6d08f8a40b6f562219
Sal <codelur@gmail.com>
2024-12-07 19:48:14 -0800

    Adding header

--- se_project_react/src/components/Header/Header.jsx
+++ se_project_react/src/components/Header/Header.jsx
@@ -1,5 +1,20 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.svg";
+
 function Header() {
-  return <header>HEADER</header>;
+  return (
+    <header className="header">
+      <img src={logo} alt="" className="header__logo" />
+      <p className="header__date-and-location">DATE, LOCATION</p>
+
+      <button className="header__add-clothes-btn">+ Add clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
